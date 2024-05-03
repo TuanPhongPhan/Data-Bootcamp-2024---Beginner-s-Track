2b7610013618df185e140d110db1b079a9810e37
Tuan Phong Phan <tuanphong.de@gmail.com>
2024-05-03 15:03:05 +0200

    small changes

--- Charts/Total watch time for each month.py
+++ Charts/Total watch time for each month.py
@@ -7,17 +7,12 @@ warnings.filterwarnings("ignore")
 # Load data from csv file
 df = pd.read_csv('../Netflix.csv', sep=';')
 
-# Helper column for various plots
-df['count'] = 1
-
 # Format the 'Duration' column and Start Time column
 df['Duration'] = pd.to_timedelta(df['Duration'])
 df['Start Time'] = pd.to_datetime(df['Start Time'])
 
 # Filter the data for 2022
 data_2022 = df[df['Start Time'].dt.year == 2022]
-data = df.groupby('Duration')['count'].sum().sort_values(ascending=False)[:5]
-
 monthly_duration_2022 = data_2022.groupby(data_2022['Start Time'].dt.month)['Duration'].sum().sort_values(
     ascending=False)
 monthly_duration_2022 = monthly_duration_2022.dt.total_seconds() / 3600
@@ -32,7 +27,8 @@ ax.bar(monthly_duration_2022.index, monthly_duration_2022, width=0.5,
        edgecolor='darkgray',
        linewidth=0.6, color=color_map)
 
-# annotations
+# Annotations
+
 for i in monthly_duration_2022.index:
     ax.annotate(f"{round(monthly_duration_2022[i])}",
                 xy=(i, monthly_duration_2022[i] + 2),
@@ -64,6 +60,6 @@ plt.xlabel("Month", fontsize=12, fontweight='light', fontfamily='serif', loc='ce
 plt.ylabel("Duration (Hours)", fontsize=12, fontweight='light', fontfamily='serif', loc='center', x=-1.5)
 
 # Show plot
-ax.tick_params(axis=u'both', which=u'both', length=0)
+ax.tick_params(axis='both', which='both', length=0)
 
 plt.show()
