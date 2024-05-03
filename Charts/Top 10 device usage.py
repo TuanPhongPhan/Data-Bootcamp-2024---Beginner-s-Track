0b9dfdd7ce622352bf07a7cd4d134bca51d8587d
Tuan Phong Phan <tuanphong.de@gmail.com>
2024-05-03 15:15:31 +0200

    small changes

--- Charts/Top 10 device usage.py
+++ Charts/Top 10 device usage.py
@@ -20,34 +20,42 @@ df['Device Type'].replace('Android DefaultWidevineL3Tablet Android Tablet', 'And
 
 data = df['Device Type'].value_counts().sort_values(ascending=False)[:10]
 
+
 color_map = ['#221f1f' for _ in range(11)]
 color_map[0] = color_map[1] = '#ff5722'  # color highlight
 
-# initialize the figure
+# Initialize the figure
+
 plt.figure(figsize=(8, 8))
 ax = plt.subplot(111, polar=True)
 plt.axis('off')
 
 # Constants = parameters controlling the plot layout:
+
 upperLimit = 1400
 lowerLimit = 90
 labelPadding = 30
 
 # Compute max in the dataset
+
 max = data.max()
 
 # Compute heights: they are a conversion of each item value in those new coordinates
+
 slope = (max - lowerLimit) / max
 heights = slope * data.values + lowerLimit
 
 # Compute the width of each bar. In total, we have 2*Pi = 360°
+
 width = 2 * np.pi / len(data.index)
 
 # Compute the angle each bar is centered on:
+
 indexes = list(range(1, len(data.index) + 1))
 angles = [element * width for element in indexes]
 
 # Draw bars
+
 bars = ax.bar(
     x=angles,
     height=heights,
@@ -59,12 +67,15 @@ bars = ax.bar(
 )
 
 # Add labels
+
 for bar, angle, height, label in zip(bars, angles, heights, data.index):
 
     # Labels are rotated. Rotation must be specified in degrees
+
     rotation = np.rad2deg(angle)
 
     # Flip some labels upside down
+
     alignment = ""
     if np.pi / 2 <= angle < 3 * np.pi / 2:
         alignment = "right"
@@ -72,7 +83,8 @@ for bar, angle, height, label in zip(bars, angles, heights, data.index):
     else:
         alignment = "left"
 
-    # Finally add the labels
+    # Add the labels
+
     ax.text(
         x=angle,
         y=lowerLimit + bar.get_height() + labelPadding,
